5ad5fc75b5789b04e88c358e05c5050d7e533544
asky123 <asky12394@gmail.com>
2021-07-21 20:40:25 +0900

    #while, for문 복습 #구구단

--- Ex_1.py
+++ Ex_1.py
@@ -8,6 +8,7 @@ while number<10:
     print("2 x %d = %d" % (number,2*number))
     number+=1
 print('------------------------------')
+
 #for 문으로 구구단 !
 for a in range(1,9):
     a+=1
@@ -16,3 +17,8 @@ for a in range(1,9):
         i += 1
         print(a,"x",i,"=",a*i)
 
+#-----------------------------------------------
+for a in range(2,10):
+    print('------------------------------') #보기 좋게 추가
+    for i in range(1,10):
+        print(a,"x",i,"=",a*i)
