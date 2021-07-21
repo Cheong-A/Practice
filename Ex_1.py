8458c8df394276b6bf5f79bc64befb205de84ddc
asky123 <asky12394@gmail.com>
2021-07-21 20:14:47 +0900

    while, for문 복습 !

---
+++ Ex_1.py
@@ -0,0 +1,16 @@
+
+#전설의 구구단 만들기
+#21.07.21_20:00
+
+#2단을 while문으로
+number = 1
+while number<10:
+    print("2 x %d = %d" % (number,2*number))
+    number+=1
+print('------------------------------')
+#for 문으로 구구단 !
+for a in range(1,9):
+    a+=1
+    for i in range(9):
+        i += 1
+        print(a,"x",i,"=",a*i)
