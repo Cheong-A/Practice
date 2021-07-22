b08f9b90f2469f29e011c5dc026aa4baa1b90d09
asky123 <asky12394@gmail.com>
2021-07-22 22:44:11 +0900

    #21.07.22 #함수 연습

---
+++ ex_Function.py
@@ -0,0 +1,15 @@
+#21.07.22
+#함수를 창조해 보자 !!
+
+#한글 함수명도 가능
+def 더하기 (a,b):
+    return print(a+b)
+
+a = int(input("정수입력:"))
+b = int(input("정수입력:"))
+더하기(a,b)
+
+#lambda로 바꾸면?
+add= lambda a, b: a+b
+
+print(add(5,6))
