ef28841e594c61795970e4051319e02ebc563cbf
asky123 <asky12394@gmail.com>
2021-08-01 13:56:48 +0900

    #21.08.01

---
+++ Crawling_실습2.py
@@ -0,0 +1,40 @@
+#네이버 영화 랭킹 크롤링
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+
+def crawler(sort1, day):
+        if sort1 in '0':
+            sort1 = 'cnt'
+        elif sort1 in '1':
+            sort1='cur'
+        else:
+            sort1 ='pnt'
+        url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=' + sort1 + '&date=' + day
+        print(url)
+
+        data = requests.get(url, headers=headers)
+        soup = BeautifulSoup(data.text, 'html.parser')
+        movies = soup.select("#old_content > table > tbody > tr")
+        for movie in movies:
+            movie_name = movie.select_one("td.title > div > a")
+            movie_point = movie.select_one("td.point")
+            if movie_name is not None:
+                ranking = movie.select_one("td:nth-child(1) > img")["alt"]
+                print(ranking, movie_name.text, movie_point.text)
+
+
+# 메인함수
+def main():
+    info_main = input("=" * 50 + "\n" + "입력 형식에 맞게 입력해주세요." + "\n" + " 시작하시려면 Enter를 눌러주세요." + "\n" + "=" * 50)
+    sort1 = input("영화 랭킹 검색 방식 입력(조회순=0  평점순(현재상영작)=1  평점순(모든영화)=2): ")
+    day = input("기준날짜 입력(20190104):")
+
+    crawler(sort1, day)
+
+
+# 메인함수 수행
+main()
