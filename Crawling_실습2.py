eff2fc593b757bcbfae975274b53a7036283c21b
asky123 <asky12394@gmail.com>
2021-08-02 14:36:38 +0900

    #21.08.02

--- Crawling_실습2.py
+++ Crawling_실습2.py
@@ -7,18 +7,17 @@ headers = {
 
 
 def crawler(sort1, day):
-        if sort1 in '0':
-            sort1 = 'cnt'
-        elif sort1 in '1':
+        if sort1 in '1':
             sort1='cur'
         else:
             sort1 ='pnt'
-        url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=' + sort1 + '&date=' + day
+        url = 'https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=' + sort1 + '&date=' + day
         print(url)
 
         data = requests.get(url, headers=headers)
         soup = BeautifulSoup(data.text, 'html.parser')
         movies = soup.select("#old_content > table > tbody > tr")
+
         for movie in movies:
             movie_name = movie.select_one("td.title > div > a")
             movie_point = movie.select_one("td.point")
@@ -30,7 +29,7 @@ def crawler(sort1, day):
 # 메인함수
 def main():
     info_main = input("=" * 50 + "\n" + "입력 형식에 맞게 입력해주세요." + "\n" + " 시작하시려면 Enter를 눌러주세요." + "\n" + "=" * 50)
-    sort1 = input("영화 랭킹 검색 방식 입력(조회순=0  평점순(현재상영작)=1  평점순(모든영화)=2): ")
+    sort1 = input("영화 랭킹 평점순 기준 입력(현재상영작=1  모든영화=2): ")
     day = input("기준날짜 입력(20190104):")
 
     crawler(sort1, day)
