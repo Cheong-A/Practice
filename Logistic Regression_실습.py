9fa5f1c50291d38b81d1ffc962ee680b8f00fac2
asky123 <asky12394@gmail.com>
2021-08-27 14:17:45 +0900

    #21.08.27

--- Logistic Regression_실습.py
+++ Logistic Regression_실습.py
@@ -19,3 +19,10 @@ from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
 # 시각화
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+
+#실습 1 : Breast Cancer
+#반응변수 : 양성여부 (malignant : 악성-0, Benign : 양성-1)
+breast_cancer = pd.read_csv(../)
+breast_cancer = load_breast_cancer()
+print(breast_cancer.DESCR)
